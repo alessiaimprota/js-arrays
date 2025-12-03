08d836bc9e2938d5b8a335945dbe49add2b9bc10
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-03 17:47:09 +0100

    documenti formattati perché sono messy di natura e non voglio rendere ciechi i tutor

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,12 +1,4 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
@@ -15,8 +7,8 @@ console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
-//const change= teachers.splice(4, 1, "Patrick"); primo tentativo, ma Donato mi ha illuminata 
-teachers[4]="Patrick"
+//const change= teachers.splice(4, 1, "Patrick"); primo tentativo, ma Donato mi ha illuminata
+teachers[4] = "Patrick";
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
@@ -27,21 +19,21 @@ console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
-console.log(firstTeacher)
-console.log(teachers)
+console.log(firstTeacher);
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-teachers.push("Vanessa")
-console.log(teachers)
+teachers.push("Vanessa");
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-teachers.unshift("Sarah")
-console.log(teachers)
+teachers.unshift("Sarah");
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = teachers.indexOf("Lewis")
-console.log(lewisIndex)
+const lewisIndex = teachers.indexOf("Lewis");
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -1,52 +1,42 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = teachers.reverse();
-console.log(reversedTeachers)
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = [];
 
-for(let i=0; i<teachers.length; i++){
-  let newName= teachers[i]
-  if(newName.length >= 5){
+for (let i = 0; i < teachers.length; i++) {
+  let newName = teachers[i];
+  if (newName.length >= 5) {
     //console.log(newName)
-    longNames.push(newName)
+    longNames.push(newName);
   }
 }
-console.log(longNames)
-
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 //teachers.splice(1,1); ho dovuto rifarlo perché ora essendo in reverse ed ha cambiato posto
-teachers.splice( 5, 1)
-console.log(teachers)
+teachers.splice(5, 1);
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 
 let fabioPresent = false;
-for (let i = 0; i<teachers.length; i++){
-  if(teachers[i]==="Fabio"){
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === "Fabio") {
     fabioPresent = true;
   }
 }
-console.log(fabioPresent)
+console.log(fabioPresent);
 
-const isFabioPresent = teachers.includes("Fabio")
-console.log(isFabioPresent)
-
+const isFabioPresent = teachers.includes("Fabio");
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.join();
-console.log(teachersString)
+console.log(teachersString);
