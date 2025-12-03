d937a007390481da4b011ec629814b331da9a534
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-03 16:23:01 +0100

    fix

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -14,7 +14,9 @@ const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-const change= teachers.splice(4, 1, "Patrick");
+
+//const change= teachers.splice(4, 1, "Patrick"); primo tentativo, ma Donato mi ha illuminata 
+teachers[4]="Patrick"
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
