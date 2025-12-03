c7fb9ccb7dc7d6a8a9ba5fe1626d5de08280120e
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-03 17:37:32 +0100

    done

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -35,15 +35,16 @@ console.log(teachers)
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 
-const isFabioPresent=false;
-console.log(isFabioPresent)
+let isFabioPresent = false;
 
 for (let i = 0; i<teachers.length; i++){
   if(teachers[i]==="Fabio"){
     isFabioPresent = true;
   }
-  console.log(isFabioPresent)
 }
+console.log(isFabioPresent)
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join();
+console.log(teachersString)
